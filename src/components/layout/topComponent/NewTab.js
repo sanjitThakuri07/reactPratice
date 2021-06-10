163448da18092baa85675b506b6f187fe7b30070
Sanjit01 <sanjitthakuri6750@gmail.com>
2021-06-10 20:39:43 +0545

    New tab add and remove functionality

--- src/components/layout/topComponent/NewTab.js
+++ src/components/layout/topComponent/NewTab.js
@@ -5,11 +5,23 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import fireFox from "../../../assests/img/firefox.png";
 
 const NewTab = (props) => {
+  // remove
+  const removeNewTabHandler = (e) => {
+    e.preventDefault();
+    props.setNewTab(
+      props.newTab.filter((del) => {
+        return del.id !== props.tab.id;
+      })
+    );
+  };
+
   return (
     <div className="active__tab utility__tab-Padding">
       <img src={fireFox} alt="" style={{ width: "1.6rem" }} />
       <p className="active-tab__name">{props.tabName}</p>
-      <FButton iconName={faTimes} />
+      <button onClick={removeNewTabHandler}>
+        <i class="fas fa-times"></i>
+      </button>
     </div>
   );
 };
--- src/components/layout/topComponent/TabContainer.js
+++ src/components/layout/topComponent/TabContainer.js
@@ -1,16 +1,41 @@
-import FButton from "../../UI/Button/fontAwesomeButton";
-
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
 
 import NewTab from "./NewTab.js";
 import "./newTab.css";
 
 const TabContainer = (props) => {
+  const [newTab, setNewTab] = useState([]);
+
+  const newTabHandler = (e) => {
+    e.preventDefault();
+    setNewTab((prevTabDetails) => {
+      return [
+        ...prevTabDetails,
+        { id: Math.random() * 100, tabName: "New Tab", key: Math.random() * 2 },
+      ];
+    });
+  };
+
+  console.log(newTab);
+
   return (
     <div className="tab__container">
-      <NewTab tabName={props.tabName} />
+      {newTab.map((tab) => {
+        return (
+          <NewTab
+            tabName={tab.tabName}
+            key={tab.key}
+            tab={tab}
+            setNewTab={setNewTab}
+            newTab={newTab}
+          />
+        );
+      })}
+      {/* <NewTab tabName={props.tabName} /> */}
       <div className="tab utility__tab-Padding">
-        <FButton iconName={faPlus} />
+        <button onClick={newTabHandler}>
+          <i class="fas fa-plus"></i>
+        </button>
       </div>
     </div>
   );
